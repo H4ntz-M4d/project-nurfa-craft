adbd17f83c0826bd48650ce281c7ea866b845cd8
H4ntz-M4d <muhadiahmad71@gmail.com>
2025-06-02 16:14:30 +0700

    add Proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
+        // Jika ada X-Forwarded-Proto header, paksa HTTPS
+        if (request()->hasHeader('X-Forwarded-Proto') && 
+            request()->header('X-Forwarded-Proto') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
